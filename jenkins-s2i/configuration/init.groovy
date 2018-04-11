16923456f5b43060fd20736d2da25af3dd8e04ff
donal <donalspring@gmail.com>
2018-04-11 11:24:46 +0100

    FIXES based on robs run through

--- jenkins-s2i/configuration/init.groovy
+++ jenkins-s2i/configuration/init.groovy
@@ -9,8 +9,8 @@ import hudson.plugins.sshslaves.*;
 
 domain = Domain.global()
 store = Jenkins.instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
-gitUsername = System.getenv("GIT_USERNAME") ?: "git"
-gitPassword = System.getenv("GIT_PASSWORD") ?: "gitpass"
+gitUsername = System.getenv("GIT_USERNAME") ?: "<USERNAME>"
+gitPassword = System.getenv("GIT_PASSWORD") ?: "<PASSWORD>"
 
 usernameAndPassword = new UsernamePasswordCredentialsImpl(
   CredentialsScope.GLOBAL,
